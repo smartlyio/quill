cad61d67de3b11bcea770fd927a236198dc17218
Mikko Kivelä <mikko@smartly.io>
2022-06-29 23:54:39 +0300

    Remove forced default formats and attributors
    We don’t really want to use Quill’s default formats and attributors, but instead want to have full control over what formats and attributors are used. There is no easy way to disabled default formats and attributors via the Quill API, but they can be easily re-registered if needed.

--- quill.js
+++ quill.js
@@ -1,30 +1,5 @@
 import Quill from './core';
 
-import { AlignClass, AlignStyle } from './formats/align';
-import { DirectionAttribute, DirectionClass, DirectionStyle } from './formats/direction';
-import { IndentClass as Indent } from './formats/indent';
-
-import Blockquote from './formats/blockquote';
-import Header from './formats/header';
-import List, { ListItem } from './formats/list';
-
-import { BackgroundClass, BackgroundStyle } from './formats/background';
-import { ColorClass, ColorStyle } from './formats/color';
-import { FontClass, FontStyle } from './formats/font';
-import { SizeClass, SizeStyle } from './formats/size';
-
-import Bold from './formats/bold';
-import Italic from './formats/italic';
-import Link from './formats/link';
-import Script from './formats/script';
-import Strike from './formats/strike';
-import Underline from './formats/underline';
-
-import Image from './formats/image';
-import Video from './formats/video';
-
-import CodeBlock, { Code as InlineCode } from './formats/code';
-
 import Formula from './modules/formula';
 import Syntax from './modules/syntax';
 import Toolbar from './modules/toolbar';
@@ -38,54 +13,7 @@ import Tooltip from './ui/tooltip';
 import BubbleTheme from './themes/bubble';
 import SnowTheme from './themes/snow';
 
-
 Quill.register({
-  'attributors/attribute/direction': DirectionAttribute,
-
-  'attributors/class/align': AlignClass,
-  'attributors/class/background': BackgroundClass,
-  'attributors/class/color': ColorClass,
-  'attributors/class/direction': DirectionClass,
-  'attributors/class/font': FontClass,
-  'attributors/class/size': SizeClass,
-
-  'attributors/style/align': AlignStyle,
-  'attributors/style/background': BackgroundStyle,
-  'attributors/style/color': ColorStyle,
-  'attributors/style/direction': DirectionStyle,
-  'attributors/style/font': FontStyle,
-  'attributors/style/size': SizeStyle
-}, true);
-
-
-Quill.register({
-  'formats/align': AlignClass,
-  'formats/direction': DirectionClass,
-  'formats/indent': Indent,
-
-  'formats/background': BackgroundStyle,
-  'formats/color': ColorStyle,
-  'formats/font': FontClass,
-  'formats/size': SizeClass,
-
-  'formats/blockquote': Blockquote,
-  'formats/code-block': CodeBlock,
-  'formats/header': Header,
-  'formats/list': List,
-
-  'formats/bold': Bold,
-  'formats/code': InlineCode,
-  'formats/italic': Italic,
-  'formats/link': Link,
-  'formats/script': Script,
-  'formats/strike': Strike,
-  'formats/underline': Underline,
-
-  'formats/image': Image,
-  'formats/video': Video,
-
-  'formats/list/item': ListItem,
-
   'modules/formula': Formula,
   'modules/syntax': Syntax,
   'modules/toolbar': Toolbar,
